05c6fde3a5a5f141aca59e4b32f62986062358e9
skakanka <kondracka.anna@gmail.com>
2016-12-06 21:03:00 -0800

    done with recursion and sorting but merge_sort

--- sorting.py
+++ sorting.py
@@ -11,6 +11,7 @@ def bubble_sort(lst):
         for j in range(len(lst)-1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
 
 
 def merge_lists(list1, list2):
